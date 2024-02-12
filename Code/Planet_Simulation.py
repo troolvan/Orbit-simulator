39ec91e5f5bc28266e4ed413d5aff445a2029da6
troolvan <ceiv.sanchezme@gmail.com>
2024-02-11 21:01:59 -0500

    Second commit

--- Code/Planet_Simulation.py
+++ Code/Planet_Simulation.py
@@ -1,13 +1,13 @@
 # Code made by troolvo
 
 import pygame
-
 from math import sin, cos
 from sys import exit
 
-# Custom events
+# Custom libraries 
 import Planets
 
+# Pygame init
 pygame.init()
 
 Width = 800
@@ -20,63 +20,44 @@ Icon = pygame.display.set_icon(Image_Icon)
 
 Font = pygame.font.Font("Assets\\Pixelify.ttf", 20)
 
-# Planets and planet constants
-
-Distance1, Distance2 = 200, 120
-Speed = 0.0005
-Direction = 1
+# Planets models
 
 Planet1 = Planets.Planet(400, 250, 50, (247, 152, 100), 0)
-Planet2 = Planets.Planet(Planet1.Xpos + Distance1, 250, 20, (23, 220, 100), 0)
-Planet3 = Planets.Planet(Planet1.Xpos + 120, 250, 20, (106, 13, 173), 0)
-
-Planet_Distance = Planets.GetDistance(Planet1, Planet2)
-Planet_Distance1 = Planets.GetDistance(Planet1, Planet3)
-
-Graph = Planets.Planet(400, 250, Planet_Distance, (255, 255, 255), 3)
-Graph1 = Planets.Planet(400, 250, Planet_Distance1, (255, 255, 255), 3)
-
+Planet2 = Planets.Planet(400, 250, 20, (23, 220, 100), 200)
+Planet3 = Planets.Planet(400, 250, 20, (106, 13, 173), 120)
 
 IsRunnning = True
 while IsRunnning:
+
     Screen.fill((10, 52, 99))
 
     # Planet rotation
-    
+
     ticks = pygame.time.get_ticks()
-    Xmove = sin(ticks * Speed)
-    Ymove = cos(ticks * Speed)
-
-    Planet2.Xpos = Planet1.Xpos + (Xmove * Distance1)
-    Planet2.Ypos = Planet1.Ypos + (Ymove * Distance1)
-
-    Planet3.Xpos = Planet1.Xpos + (-Xmove * Distance2)
-    Planet3.Ypos = Planet1.Ypos + (Ymove * Distance2)
+    Planet2.MovePlanet(ticks)
+    Planet3.MovePlanet(-ticks)
 
     # Texts
-    XText = Font.render("X Pos: " + str(round(Xmove, 2)), False, (255, 255, 255))
-    YText = Font.render("Y Pos: " + str(round(Ymove, 2)), False, (255, 255, 255))
+    
+    Planet_1 = "({0}, {1})".format(round(Planet2.Xpos, 1), round(Planet2.Ypos, 1))
+    Planet_2 = "({0}, {1})".format(round(Planet3.Xpos, 1), round(Planet3.Ypos, 1))
+
+    XText = Font.render("Planet[0] Pos: " + Planet_1, False, (255, 255, 255))
+    YText = Font.render("Planet[1] Pos: " + Planet_2 , False, (255, 255, 255))
 
     # Keybinds events
+
     Events = pygame.event.get()
     for event in Events:
         if event.type == pygame.QUIT:
             IsRunnning = False
             exit()
 
-        """
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                Direction *= -1
-        """
-
     # Update Planets
-    Graph.Update(Screen)
-    Graph1.Update(Screen)
-
-    Planet1.Update(Screen)
-    Planet2.Update(Screen)
-    Planet3.Update(Screen)
+        
+    Planet1.Update(Screen, False)
+    Planet2.Update(Screen, True)
+    Planet3.Update(Screen, True)
 
     Screen.blit(XText, (10, 10))
     Screen.blit(YText, (10, 40))
@@ -84,3 +65,4 @@ while IsRunnning:
     pygame.display.update()
 
 pygame.quit()
+
--- Code/Planets.py
+++ Code/Planets.py
@@ -2,23 +2,26 @@ import pygame
 from math import sqrt, pow, sin, cos
 
 class Planet:
-    def __init__(self, Xpos, Ypos, Ratius, Color, Outline):
+    def __init__(self, Xpos, Ypos, Ratius, Color, Distance):
         self.Xpos = Xpos
         self.Ypos = Ypos
+
         self.Ratius = Ratius
+        self.Distance = Distance
+        self.Speed = 0.0002
+
         self.Color = Color
 
-        self.Outline = Outline
+    def MovePlanet(self, tick):
+        self.Xmove = sin(tick * self.Speed)
+        self.Ymove = cos(tick * self.Speed)
 
+        self.Xpos = 400 + (self.Xmove * self.Distance)
+        self.Ypos = 250 + (self.Ymove * self.Distance)
 
-    def Update(self, Screen):
-        self.Rect = pygame.draw.circle(Screen, self.Color, (self.Xpos, self.Ypos), self.Ratius, self.Outline)
+    def Update(self, Screen, orbit):
+        if orbit:
+            self.Graph = pygame.draw.circle(Screen, (255, 255, 255), (400, 250), self.Distance, 3)
 
-
-def GetDistance(Planet1, Planet2):
-
-    return sqrt(pow((Planet2.Xpos - Planet1.Xpos), 2) + pow((Planet2.Ypos - Planet1.Ypos), 2))
-
-
-
+        self.Rect = pygame.draw.circle(Screen, self.Color, (self.Xpos, self.Ypos), self.Ratius)
 
